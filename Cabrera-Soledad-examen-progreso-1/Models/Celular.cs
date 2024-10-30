97f5eb0e976ee6f06e06ffd3c6c3aa5ab534b865
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2024-10-30 08:50:01 -0500

    Modelo Celular completo, o eso creo.

---
+++ Cabrera-Soledad-examen-progreso-1/Models/Celular.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cabrera_Soledad_examen_progreso_1.Models
+{
+    public class Celular
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        
+        [StringLength(100)]
+        public string Modelo { get; set; }
+        
+        [MaxLength(10000)]
+        public int año  { get; set; }
+
+        [Range(1, 5000, ErrorMessage = "El precio debe ser entre $1 y $5000")]
+        public decimal precio { get; set; }
+    }
+}
