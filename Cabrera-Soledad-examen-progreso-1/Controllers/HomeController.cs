11a2f031474c41e97b76f42883655dee03ba8108
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2024-10-30 08:23:55 -0500

    Agregar archivos de proyecto.

---
+++ Cabrera-Soledad-examen-progreso-1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Cabrera_Soledad_examen_progreso_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cabrera_Soledad_examen_progreso_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cabrera-Soledad-examen-progreso-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cabrera_Soledad_examen_progreso_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
