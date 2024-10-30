d8ad7fb51177aceb910e5e77874fe176677bc5aa
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2024-10-30 14:19:26 -0500

    funcionamiento de la alerta y padding a la imagen

--- Cabrera-Soledad-examen-progreso-1/wwwroot/js/site.js
+++ Cabrera-Soledad-examen-progreso-1/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-funcion MasSobreMi(){
+function MasSobreMi(){
     alert("Hola, mi nombre es Soledad Cabrera y mi hobby es leer (pero solo cosas divertidas porque, si no, me aburro).")
 }
 // Write your JavaScript code.
